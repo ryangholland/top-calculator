a8a2560ee8c9782964c7efb2cafefb0a74c6d276
ryangholland <rghcode@gmail.com>
2024-06-04 09:00:13 -0400

    Add basic calculator functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(add(4, 2))
+console.log(subtract(4, 2))
+console.log(multiply(4, 2))
+console.log(divide(4, 2))
