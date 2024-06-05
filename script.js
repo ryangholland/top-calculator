7fcdf7f833e84231ec1b18e65cecfd92fbe79702
ryangholland <rghcode@gmail.com>
2024-06-05 07:30:21 -0400

    Add initial functionality for operator and equals buttons

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const numButtons = document.querySelectorAll(".num-btn");
 const operatorButtons = document.querySelectorAll(".op-btn");
+const equalButton = document.querySelector(".eq-btn");
 const displayTop = document.querySelector(".display-top");
 const displayBottom = document.querySelector(".display-bottom");
+
+let num1 = null;
+let num2 = null;
+let operator = null;
 
 function add(num1, num2) {
   return num1 + num2;
 }
-``;
+
 function subtract(num1, num2) {
   return num1 - num2;
 }
@@ -23,23 +28,39 @@ function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
       return add(num1, num2);
-    case "-":
+    case "−":
       return subtract(num1, num2);
-    case "*":
+    case "×":
       return multiply(num1, num2);
-    case "/":
+    case "÷":
       return divide(num1, num2);
   }
 }
 
 numButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    displayBottom.innerText == "0"
+    displayBottom.innerText == "0" || num1
       ? (displayBottom.innerText = e.target.innerText)
       : (displayBottom.innerText += e.target.innerText);
   });
 });
 
-let num1 = null;
-let num2 = null;
-let operator = null;
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+
+    // behave differently if num1 already exists?
+
+    num1 = displayBottom.innerText;
+    operator = e.target.innerText;
+
+    console.log(num1);
+    console.log(operator);
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  if (num1 && operator) {
+    num2 = displayBottom.innerText;
+    displayBottom.innerText = operate(operator, +num1, +num2);
+  }
+});
